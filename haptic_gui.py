85553a1f4ff2d61a8b7a3c8e2502cf3dc46595e4
kaoweitse <kaoweitse@lolab-nthu.org>
2018-10-09 20:49:34 +0800

    Resize window size and add window title

--- haptic_gui.py
+++ haptic_gui.py
@@ -12,45 +12,22 @@ class Example(Frame):
         self.createWidgets()
 
     def say_hi(self):
-        print "hi there, everyone! "
-        #print "Name: ", self.name.get()
-        #print "Password: ", self.password.get()
-        #self.password.delete(0, 'end')
+        print "hi there, hello world! "
 
     def say_halo(self):
         print "Moin! Guten Tag! "
 
-
     def createWidgets(self):
+        self.master.title("hapticGUI")
+
         self.QUIT = Button(self)
         self.QUIT["text"] = "QUIT"
         self.QUIT["fg"]   = "red"
         self.QUIT["command"] =  self.quit
-
         self.QUIT.pack({"side": "left"})
-        '''
-        # Simple Label widget:
-        self.name_title = Label(self, text="Name:")
-        self.name_title.pack({"side": "left"})
-        
-        # Simple Entry widget:
-        self.name = Entry(self)
-        self.name.pack({"side": "left"})
-        self.name.insert(0, "Your name")
-        
-        # Simple Label widget:
-        self.password_title = Label(self, text="Password:")
-        self.password_title.pack({"side": "left"})
 
-        # In order to hide the text as it is typed (e.g. for Passwords)
-        # set the "show" parameter:
-        self.password = Entry(self)
-        self.password["show"] = "*"
-        self.password.pack({"side": "left"})
-        '''
         self.hi_there = Button(self)
         self.hi_there["text"] = "Hello",
-
         self.hi_there["command"] = self.say_hi
         self.hi_there.bind("<Enter>", self.on_enter)
         #self.hi_there.bind("<Leave>", self.on_leave)
@@ -58,28 +35,23 @@ class Example(Frame):
 
         self.guten_tag = Button(self)
         self.guten_tag["text"] = "GutenTag",
-
         self.guten_tag["command"] = self.say_halo
-        #self.hi_there.bind("<Enter>", self.on_enter)
-        #self.hi_there.bind("<Leave>", self.on_leave)
         self.guten_tag.pack({"side": "left"})
 
     def on_enter(self, event):
-        print "hi there, hello world! "
-        #self.l2.configure(text="Hello world")
+        print "bzzzzzzzzzz~ "
         ser1.write('s')
 
     #def on_leave(self, enter):
-        #self.l2.configure(text="")
+        #print "bzzzzzzzzzz~ "
 
 def main():
     root = Tk()
     app = Example(parent=root)
-
     root.lift()
     root.call('wm', 'attributes', '.', '-topmost', True)
     root.after_idle(root.call, 'wm', 'attributes', '.', '-topmost', False)
-
+    root.geometry("1200x800")
     app.mainloop()
 
 main()
