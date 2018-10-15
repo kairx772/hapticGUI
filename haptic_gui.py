02a5a41a0b844cbded9f73f9a1ddbbcb158708fa
kairx772 <kaoweitse@loalb-nthu.org>
2018-10-15 12:13:28 +0200

    Click target to creat new target

--- haptic_gui.py
+++ haptic_gui.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import tkinter as tk
-import serial as ser
+from tkinter import ttk
+import random
 
 #ser1 = ser.Serial('/dev/ttyUSB0',9600)
 #ser1.write('s')
@@ -24,15 +25,27 @@ class MainApplication(tk.Frame):
 
     # Creating the square frame
     def create_squareframe(self):
-        self.square_frame = tk.Frame(width=100, height=100, background='red')
+        targetsize = random.randint(5,100)
+        self.square_frame = tk.Frame(width=targetsize, height=targetsize, background='red')
         self.square_frame.grid(column=0, row=0)
         self.square_frame.bind("<Enter>", self.on_enter)
-        self.square_frame.bind("<Button-1>", self.leftclick)
-        self.square_frame.place(x=200, y=200)
+        self.square_frame.bind("<Button-1>", self.check_info)
+        self.square_frame.place(x=random.randint(30,500), y=random.randint(30,500))
         #self.square_frame.pack(expand=True, fill=tk.BOTH)
         #self.left_frame = tk.Frame(width=100, height=100, background='red')
         #self.left_frame.grid_propagate(0)
         #self.square_frame.grid(row=0, column=0)
+
+    def check_info(self, event):
+        targetsize = random.randint(5,100)
+        self.square_frame = tk.Frame(width=targetsize, height=targetsize, background='red')
+        self.square_frame.grid(column=0, row=0)
+        self.square_frame.bind("<Enter>", self.on_enter)
+        self.square_frame.bind("<Button-1>", self.check_info)
+        self.square_frame.place(x=random.randint(0,1000), y=random.randint(0,1000))
+        #self.resizable(False, False)
+
+        #ttk.Button(self.info_frame, text="Return Home", command=self.goback).grid(column=2, row=2, sticky=(W, E))
 
     def say_hi(self):
         print ('hi there, hello world! ')
