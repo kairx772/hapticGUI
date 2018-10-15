32745828608343ca5494586c369ab3d26ff2c699
kairx772 <kaoweitse@loalb-nthu.org>
2018-10-15 11:10:30 +0200

    Frame as evet

--- haptic_gui.py
+++ haptic_gui.py
@@ -27,24 +27,19 @@ class MainApplication(tk.Frame):
     def create_squareframe(self):
         self.square_frame = tk.Frame(width=100, height=100, background='red')
         self.square_frame.grid(column=0, row=0)
-
+        self.square_frame.bind("<Enter>", self.on_enter)
+        self.square_frame.bind("<Button-2>", self.say_hi)
+        self.square_frame.place(x=200, y=200)
+        #self.square_frame.pack(expand=True, fill=tk.BOTH)
         #self.left_frame = tk.Frame(width=100, height=100, background='red')
         #self.left_frame.grid_propagate(0)
         #self.square_frame.grid(row=0, column=0)
 
-
-    def create_buttons(self):
-        self.button1 = tk.Button(text='Button1')
-        self.button1.config( height = 5, width = 10 )
-        self.button1["command"] = self.say_hi
-        self.button1.bind("<Enter>", self.on_enter) 
-        self.button1.img = ''
-        #self.button1.config(height=100, width=100, image=button1.img, compound=CENTER)      
-        self.button1.place(x=600, y=400)
-
-
     def say_hi(self):
         print ('hi there, hello world! ')
+
+    def leftclick(self, event):
+        print ('left~ ')
 
     def on_enter(self, event):
         print ('bzzzzzzzzzz~ ')
