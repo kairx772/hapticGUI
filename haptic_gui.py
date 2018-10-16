01bbde66a53584da0918171d7f1f38ea6e9fddf0
kairx772 <kaoweitse@loalb-nthu.org>
2018-10-16 13:36:10 +0200

    Add click event, not yet fix the .destroy() bug.

--- haptic_gui.py
+++ haptic_gui.py
@@ -6,7 +6,10 @@ import random
 #ser1 = ser.Serial('/dev/ttyUSB0',9600)
 #ser1.write('s')
 
+
+
 class MainApplication(tk.Frame):
+
     def __init__(self, master):
         self.master = master
         tk.Frame.__init__(self, self.master)
@@ -21,7 +24,12 @@ class MainApplication(tk.Frame):
         #self.master.resizable(0, 0)
 
     def create_widgets(self):
-        self.create_squareframe()
+        temp1 = self.create_squareframe()
+        self.create_button()
+        #print (type(temp1))
+
+    def create_button(self):
+        self.erase = tk.Button(root,text='Clear',command=self.clear).grid(row=1)
 
     # Creating the square frame
     def create_squareframe(self):
@@ -30,7 +38,7 @@ class MainApplication(tk.Frame):
         self.square_frame.grid(column=0, row=0)
         self.square_frame.bind("<Enter>", self.on_enter)
         self.square_frame.bind("<Button-1>", self.check_info)
-        self.square_frame.place(x=random.randint(30,500), y=random.randint(30,500))
+        self.square_frame.place(x=random.randint(30,450), y=random.randint(30,500))
         #self.square_frame.pack(expand=True, fill=tk.BOTH)
         #self.left_frame = tk.Frame(width=100, height=100, background='red')
         #self.left_frame.grid_propagate(0)
@@ -42,10 +50,23 @@ class MainApplication(tk.Frame):
         self.square_frame.grid(column=0, row=0)
         self.square_frame.bind("<Enter>", self.on_enter)
         self.square_frame.bind("<Button-1>", self.check_info)
+        self.square_frame.bind("<Button-1>", print ('click click~'))
+        #self.square_frame.bind("<Button-1>", lambda x:self.destroy())
         self.square_frame.place(x=random.randint(0,1000), y=random.randint(0,1000))
         #self.resizable(False, False)
 
         #ttk.Button(self.info_frame, text="Return Home", command=self.goback).grid(column=2, row=2, sticky=(W, E))
+    def _close(self):
+        self.destroy()
+
+    def clear(self):
+        list = root.grid_slaves()
+        for l in list:
+            print (type(l))
+            l.destroy()
+
+    def goback(self):
+        self.info_frame.destroy()
 
     def say_hi(self):
         print ('hi there, hello world! ')
