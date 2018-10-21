4c183cf672dec99c6f744d0bb810fdd0f40bd92f
kairx772 <kaoweitse@loalb-nthu.org>
2018-10-21 15:55:19 +0200

    Create green square as function

--- haptic_gui.py
+++ haptic_gui.py
@@ -1,12 +1,154 @@
 #!/usr/bin/env python3
 import tkinter as tk
-from tkinter import ttk
+from tkinter import font  as tkfont
 import random
 
 #ser1 = ser.Serial('/dev/ttyUSB0',9600)
 #ser1.write('s')
 
 
+
+
+
+class SampleApp(tk.Tk):
+
+    def __init__(self, *args, **kwargs):
+        tk.Tk.__init__(self, *args, **kwargs)
+
+        self.title_font = tkfont.Font(family='Helvetica', size=18, weight="bold", slant="italic")
+        self.geometry('1000x800')
+
+        # the container is where we'll stack a bunch of frames
+        # on top of each other, then the one we want visible
+        # will be raised above the others
+        container = tk.Frame(self)
+        container.pack(side="top", fill="both", expand=True)
+        container.grid_rowconfigure(0, weight=0)
+        container.grid_columnconfigure(0, weight=0)
+
+        self.frames = {}
+        for F in (StartPage, PageOne, PageTwo):
+            page_name = F.__name__
+            print (F)
+            print(type(F))
+            frame = F(parent=container, controller=self)
+            self.frames[page_name] = frame
+
+            # put all of the pages in the same location;
+            # the one on the top of the stacking order
+            # will be the one that is visible.
+            frame.grid(row=0, column=0, sticky="nsew")
+
+        self.show_frame("StartPage")
+
+    def show_frame(self, page_name):
+        '''Show a frame for the given page name'''
+        frame = self.frames[page_name]
+        frame.tkraise()
+
+class StartPage(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+        label = tk.Label(self, text="This is the start page", font=controller.title_font)
+        label.pack(side="top", fill="x", pady=10)
+
+        button1 = tk.Button(self, text="Go to Page One",
+                            command=lambda: controller.show_frame("PageOne"))
+        button2 = tk.Button(self, text="Destroy",
+                            command=lambda: square_frame.destroy(self))
+        button1.pack()
+        button2.pack()
+
+
+class PageOne(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+        label = tk.Label(self, text="This is page 1", font=controller.title_font)
+        label.pack(side="top", fill="x", pady=10)
+        button1 = tk.Button(self, text="Go to the page2",
+                           command=lambda: controller.show_frame("PageTwo"))
+        button2 = tk.Button(self, text="Go to the start page",
+                           command=lambda: controller.show_frame("StartPage"))
+        self.create_squareframe()
+        button1.pack()
+        button2.pack()
+
+
+    def create_squareframe(self):
+        targetsize = random.randint(5,100)
+        self.square_frame = tk.Frame(width=targetsize, height=targetsize, background='red')
+        #self.square_frame.grid(column=0, row=0)
+        self.square_frame.bind("<Enter>", self.on_enter)
+        self.square_frame.bind("<Button-1>", lambda: controller.show_frame("PageTwo"))
+        self.square_frame.place(x=random.randint(30,450), y=random.randint(30,500))
+
+    def on_enter(self, event):
+        print ('bzzzzzzzzzz~ ')
+        #ser1.write('s')
+
+class PageTwo(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+        label = tk.Label(self, text="This is page 2", font=controller.title_font)
+        label.pack(side="top", fill="x", pady=10)
+        button1 = tk.Button(self, text="Go to the page1",
+                           command=lambda: controller.show_frame("PageOne"))
+        button2 = tk.Button(self, text="Go to the start page",
+                           command=lambda: controller.show_frame("StartPage"))        
+        self.create_squareframe()
+        create_squareframe_fn(self)
+        button1.pack()
+        button2.pack()
+    def create_squareframe(self):
+        targetsize = random.randint(5,100)
+        self.square_frame = tk.Frame(width=targetsize, height=targetsize, background='red')
+        #self.square_frame.grid(column=0, row=0)
+        self.square_frame.bind("<Enter>", self.on_enter)
+        self.square_frame.bind("<Button-1>", lambda: controller.show_frame("PageTwo"))
+        self.square_frame.place(x=random.randint(30,450), y=random.randint(30,500))
+
+    def on_enter(self, event):
+        print ('bzzzzzzzzzz~ ')
+        #ser1.write('s')
+
+
+def create_squareframe_fn(self):
+    targetsize = random.randint(5,100)
+    square_frame = tk.Frame(width=targetsize, height=targetsize, background='green')
+        #self.square_frame.grid(column=0, row=0)
+    square_frame.bind("<Enter>", on_enter_fn())
+        #self.square_frame.bind("<Button-1>", lambda: controller.show_frame("PageTwo"))
+    square_frame.place(x=random.randint(30,450), y=random.randint(30,500))
+
+def on_enter_fn():
+    print ('bzzzzzzzzzzlalalalallalal~ ')
+    #ser1.write('s')
+
+'''
+class SquareFrame(tk.Tk):
+
+    def __init__(self):
+        targetsize = random.randint(5,100)
+        tk.Frame.__init__(self, width=targetsize, height=targetsize, background='green')
+        self.place(x=random.randint(30,450), y=random.randint(30,500))
+        #self.controller = controller
+        #label = tk.Label(self, text="This is page 1", font=controller.title_font)
+        #label.pack(side="top", fill="x", pady=10)
+        #button1 = tk.Button(self, text="Go to the page2",
+                           #command=lambda: controller.show_frame("PageTwo"))
+        #button2 = tk.Button(self, text="Go to the start page",
+                           #command=lambda: controller.show_frame("StartPage"))
+        #self.create_squareframe()
+        #button1.pack()
+        #button2.pack()
+'''
+'''
 
 class MainApplication(tk.Frame):
 
@@ -29,7 +171,7 @@ class MainApplication(tk.Frame):
         #print (type(temp1))
 
     def create_button(self):
-        self.erase = tk.Button(root,text='Clear',command=self.clear).grid(row=1)
+        self.erase = tk.Button(root,text='Clear',command=clear).grid(row=1)
 
     # Creating the square frame
     def create_squareframe(self):
@@ -43,6 +185,7 @@ class MainApplication(tk.Frame):
         #self.left_frame = tk.Frame(width=100, height=100, background='red')
         #self.left_frame.grid_propagate(0)
         #self.square_frame.grid(row=0, column=0)
+        #我在衝三小朋友?
 
     def check_info(self, event):
         targetsize = random.randint(5,100)
@@ -50,9 +193,13 @@ class MainApplication(tk.Frame):
         self.square_frame.grid(column=0, row=0)
         self.square_frame.bind("<Enter>", self.on_enter)
         self.square_frame.bind("<Button-1>", self.check_info)
-        self.square_frame.bind("<Button-1>", print ('click click~'))
-        #self.square_frame.bind("<Button-1>", lambda x:self.destroy())
+        #self.square_frame.bind("<Button-1>", print ('click click~'))
+        #self.square_frame.bind("<Button-1>", lambda x:self._close())
         self.square_frame.place(x=random.randint(0,1000), y=random.randint(0,1000))
+
+
+        def close(self):
+            self.square_frame.destroy()
         #self.resizable(False, False)
 
         #ttk.Button(self.info_frame, text="Return Home", command=self.goback).grid(column=2, row=2, sticky=(W, E))
@@ -78,7 +225,16 @@ class MainApplication(tk.Frame):
         print ('bzzzzzzzzzz~ ')
         #ser1.write('s')
 
+'''
+
+'''
 if __name__ == '__main__':
    root = tk.Tk()
    main_app =  MainApplication(root)
    root.mainloop()
+'''
+
+if __name__ == "__main__":
+    app = SampleApp()
+    #sf = SquareFrame()
+    app.mainloop()
